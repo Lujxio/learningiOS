d9de757ab8aede2811692522b960c1e1c2bee913
lujxio <jcdiaz92@gmail.com>
2014-07-02 23:18:17 -0400

    Chapter 1 Quiz App

---
+++ Quiz/Quiz/JDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDAppDelegate.h
+//  Quiz
+//
+//  Created by ジュリオ ヂアドズ on 7/2/14.
+//  Copyright (c) 2014 Julio Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by ジュリオ ヂアドズ on 7/2/14.
+//  Copyright (c) 2014 Julio Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDAppDelegate class]));
+    }
+}
