1e597f60fc791d4e201107bda5a60570b12d030f
lujxio <jcdiaz92@gmail.com>
2014-07-03 00:36:05 -0400

    Finished Chap 1

--- Quiz/Quiz/JDAppDelegate.m
+++ Quiz/Quiz/JDAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "JDAppDelegate.h"
+#import "JDViewController.h"
 
 @implementation JDAppDelegate
 
@@ -14,6 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    JDViewController *quizVC = [[JDViewController alloc] init];
+    self.window.rootViewController = quizVC;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Quiz/Quiz/JDViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDViewController.h
+//  Quiz
+//
+//  Created by ジュリオ ヂアドズ on 7/2/14.
+//  Copyright (c) 2014 Julio Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDViewController : UIViewController
+
+@end
